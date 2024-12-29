b7396fb61eed5477eee29a2fd23aeb28ed7ddaa8
domi-kunz <83969066+domi-kunz@users.noreply.github.com>
2024-12-29 14:01:24 +0100

    deploy.sh push

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+#abort on errors
+set-e
+
+env PUBLIC_URL=https://domi-kunz.github.io/adi-spanien npm run build
+
+cd build
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com: domi-kunz/adi-spanien.git main:gh-pages
